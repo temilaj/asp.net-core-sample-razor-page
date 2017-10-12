8072b647dbc71fea2143a11805784c1fe24c2bc2
Temi Lajumoke <temi@temilajumoke.com>
2017-10-12 17:42:08 +0100

    initial commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace razor.Pages
+{
+    public class IndexModel : PageModel
+    {
+        [BindProperty]
+        public string Message { get; private set; } = "PageModel in C#";
+        private readonly ILogger<IndexModel> _logger;
+        public IndexModel(IConfiguration config, ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            Message += $" Server time is { DateTime.Now }";
+            _logger.LogCritical("Hey YO!");
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            return RedirectToPage("/Index");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace razor
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                // .ConfigureAppConfiguration(configBuilder => {
+                //     configBuilder.AddJsonFile("appsettings.json");
+                // })
+                // .ConfigureLogging((context, loggerFactory) => {
+                //     loggerFactory.AddConfiguration(context.Configuration);
+                //     loggerFactory.AddConsole();
+                //     loggerFactory.AddDebug();
+                // })
+                .Build();
+    }
+}
